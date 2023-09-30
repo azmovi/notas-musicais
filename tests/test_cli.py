1a4c4c216a1e8669e63ccd228c50f2bdd09b6c0f
azevedo <azevedoantoniocicero@gmail.com>
2023-09-30 16:12:33 -0300

    implementação do comando de campo harmonico

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -24,18 +24,30 @@ def test_escala_cli_deve_conter_as_notas_na_resposta_de_fa(nota):
 
 
 @mark.parametrize('grau', ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'])
-def test_escala_cli_deve_conter_as_notas_na_resposta_de_fa(grau):
+def test_escala_cli_deve_conter_todos_os_graus(grau):
     result = runner.invoke(app, ['escala', 'F'])
     assert grau in result.stdout
 
 
 @mark.parametrize('nota', ['C', 'E', 'G'])
-def test_notas_cli_deve_conter_as_notas_na_resposta_de_fa(nota):
+def test_notas_cli_deve_conter_o_acorde(nota):
     result = runner.invoke(app, ['acorde'])
     assert nota in result.stdout
 
 
 @mark.parametrize('grau', ['I', 'III', 'V'])
-def test_acorde_cli_deve_conter_as_notas_na_resposta_de_fa(grau):
+def test_acorde_cli_deve_conter_os_graus_do_acorde_fa(grau):
     result = runner.invoke(app, ['acorde', 'F'])
     assert grau in result.stdout
+
+
+@mark.parametrize('grau', ['I', 'ii', 'iii', 'IV', 'V', 'vi', 'vii°'])
+def test_campo_harmonico_cli_deve_conter_as_notas_na_respo(grau):
+    result = runner.invoke(app, ['campo-harmonico', 'C'])
+    assert grau in result.stdout
+
+
+@mark.parametrize('cifra', ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'B°'])
+def test_campo_harmonico_cli_deve_conter_todas_as_cifras(cifra):
+    result = runner.invoke(app, ['campo-harmonico', 'C'])
+    assert cifra in result.stdout
